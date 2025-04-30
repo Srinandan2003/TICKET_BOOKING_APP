67fba373ec73ee71ac44442200501ce7e63c52fb
Srinandan <srinandan2003@gmail.com>
2025-04-30 22:01:12 +0530

    Changing the Ui

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -2,9 +2,9 @@ import Login from "../components/Auth/SignIn.jsx";
 
 function LogIn() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 flex flex-col items-center justify-start">
+    <div className="min-h-screen bg-gradient-to-br flex flex-col items-center justify-start">
       {/* Intro Section */}
-      <div className="w-full bg-blue-600 text-white p-6 text-center shadow-md">
+      <div className="w-full bg-blue-800 text-white p-6 text-center shadow-md">
         <h1 className="text-3xl font-bold mb-2">Welcome to Ticket Booking</h1>
 
       </div>
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,9 +2,9 @@ import Register from "../components/Auth/SignUp.jsx";
 
 const RegisterPage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 flex flex-col items-center justify-start">
+    <div className="min-h-screen bg-gradient-to-br flex flex-col items-center justify-start">
       {/* Intro Section */}
-      <div className="w-full bg-blue-600 text-white p-6 text-center shadow-md">
+      <div className="w-full bg-blue-800 text-white p-6 text-center shadow-md">
         <h1 className="text-3xl font-bold mb-2">Join Ticket Booking</h1>
       </div>
 
