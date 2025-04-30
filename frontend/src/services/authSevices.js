438b3d8da7b06dd88d6d1fa0fc1cd4b102fd7b9a
Srinandan <srinandan2003@gmail.com>
2025-04-30 16:04:15 +0530

    Updated BASE_URL

--- frontend/src/services/authSevices.js
+++ frontend/src/services/authSevices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = "https://ticket-booking-app-pfq1.onrender.com";
 
 export const registerUser = async(data) =>{
     return await axios.post(`${BASE_URL}/api/auth/sign-Up`,data)
