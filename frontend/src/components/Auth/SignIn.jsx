3b32981db2ddfb13d50c321c97b4d58baadc0fe0
Srinandan <srinandan2003@gmail.com>
2025-04-30 16:30:19 +0530

    Updated

--- frontend/src/components/Auth/SignIn.jsx
+++ frontend/src/components/Auth/SignIn.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../../services/authSevices.js";
-import { useNavigate } from "react-router-dom";
+
 
 // Inside your Login component
 const navigate = useNavigate();
